f6373551f9178101bf9bfe470749cf53eca541b3
najeemakhtari <mohammadnajeem1996@gmail.com>
2024-04-13 19:57:31 +0330

    free course button in course.html

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,6 @@
+///////////////////////free course btn
 const titleBtn = document.querySelector(".title__btn");
+
 titleBtn.addEventListener("click", () => {
   document.querySelector(".title__btn").classList.toggle("active");
 });
